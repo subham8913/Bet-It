6c5432765c604a24d8a190bac7f1da6ee7ce181b
Sugandh-Mishra <sugandhmishra10@gmail.com>
2022-11-03 18:02:47 +0530

    test

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,26 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Board from './MonopolyBoard/Board';
 
-function App() {
-  const [backendData,setBackendData]=useState([{}])
-  // useEffect(() => {
-  //   fetch(`/api`)
-  //    .then((response) => response.json()).then(
-  //     data =>{
-  //       setBackendData(data)
-  //     }
-  //    )
-  //  }, [])
-  return (
-    <div className="App">
-    <Board/>
+// function App() {
+//   const [backendData,setBackendData]=useState([{}])
+//   // useEffect(() => {
+//   //   fetch(`/api`)
+//   //    .then((response) => response.json()).then(
+//   //     data =>{
+//   //       setBackendData(data)
+//   //     }
+//   //    )
+//   //  }, [])
+//   return (
+//     <div className="App">
+//     <Board/>
    
-   {/* {backendData.users.map((user,i) => (
+//    {/* {backendData.users.map((user,i) => (
    
-       <p>key={i} {user}</p>
-    ))}  */}
-    </div>
-  );
-}
+//        <p>key={i} {user}</p>
+//     ))}  */}
+//     </div>
+//   );
+// }
 
 export default App;
