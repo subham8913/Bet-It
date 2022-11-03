4b8df18a400b50f4b06f02efdf5d8e43f403eb7e
Sugandh-Mishra <sugandhmishra10@gmail.com>
2022-11-04 01:02:02 +0530

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Board from './MonopolyBoard/Board';
+// import { useState, useEffect } from 'react';
+// import './App.css';
+// import Board from './MonopolyBoard/Board';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./login/login";
+import Register from "./login/registration";
+import Dashboard from "./login/dashboard";
+
+const App = () => {
+	return (
+		<div>
+			<Routes>
+				<Route exact path="/" element={<Login />} />
+				<Route exact path="/register" element={<Register />} />
+				<Route exact path="/dashboard" element={<Dashboard />} />
+			</Routes>
+		</div>
+	);
+};
 
 // function App() {
-//   const [backendData,setBackendData]=useState([{}])
+//   // const [backendData,setBackendData]=useState([{}])
 //   // useEffect(() => {
 //   //   fetch(`/api`)
 //   //    .then((response) => response.json()).then(
@@ -15,9 +32,9 @@ import Board from './MonopolyBoard/Board';
 //   return (
 //     <div className="App">
 //     <Board/>
-   
+
 //    {/* {backendData.users.map((user,i) => (
-   
+
 //        <p>key={i} {user}</p>
 //     ))}  */}
 //     </div>
